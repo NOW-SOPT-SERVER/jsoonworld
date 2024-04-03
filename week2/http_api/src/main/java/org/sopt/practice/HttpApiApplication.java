8b0a30a855c7b1bc9722b32ada3337eb3fa8f9ba
권장순 <jsoonworld@gmail.com>
2024-04-03 10:50:13 +0900

    feat: prepare for 2nd seminar

---
+++ week2/http_api/src/main/java/org/sopt/practice/HttpApiApplication.java
@@ -0,0 +1,15 @@
+package org.sopt.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpApiApplication.class, args);
+	}
+
+}
+
+
