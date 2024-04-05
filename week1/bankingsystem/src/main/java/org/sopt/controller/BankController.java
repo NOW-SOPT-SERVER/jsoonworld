f39750f9fe1d45eafa72b53d02549c4f15523da5
권장순 <jsoonworld@gmail.com>
2024-04-05 16:58:56 +0900

    feat: BankController 계좌 번호 입력과 호출에 해당하는 작업 수행 기능 추가

--- week1/bankingsystem/src/main/java/org/sopt/controller/BankController.java
+++ week1/bankingsystem/src/main/java/org/sopt/controller/BankController.java
@@ -1,22 +1,13 @@
 package org.sopt.controller;
 
-import org.sopt.domain.Account;
 import org.sopt.service.AccountService;
 import org.sopt.view.input.UserInputView;
 import org.sopt.view.output.UserOutputView;
 
 public class BankController {
-    private AccountService accountService;
-    private UserInputView userInputView;
-    private UserOutputView userOutputView;
-    private Account account; // 간단한 예제를 위해 단일 계좌만 사용
-
-    public BankController(Account account, AccountService accountService, UserInputView userInputView, UserOutputView userOutputView) {
-        this.account = account;
-        this.accountService = accountService;
-        this.userInputView = userInputView;
-        this.userOutputView = userOutputView;
-    }
+    private AccountService accountService = new AccountService();
+    private UserInputView userInputView = new UserInputView();
+    private UserOutputView userOutputView = new UserOutputView();
 
     public void start() {
         boolean running = true;
@@ -24,23 +15,38 @@ public class BankController {
             int option = userInputView.getUserOption();
             switch (option) {
                 case 1: // 입금
+                    userOutputView.displayMessage("입금할 계좌의 번호를 입력해주세요:");
+                    String depositAccountNumber = userInputView.getAccountNumber();
                     double depositAmount = userInputView.getAmount();
-                    accountService.deposit(account, depositAmount);
+                    accountService.deposit(depositAccountNumber, depositAmount);
                     userOutputView.displaySuccessMessage("입금", depositAmount);
                     break;
                 case 2: // 출금
+                    userOutputView.displayMessage("출금할 계좌의 번호를 입력해주세요:");
+                    String withdrawAccountNumber = userInputView.getAccountNumber();
                     double withdrawAmount = userInputView.getAmount();
-                    if (accountService.withdraw(account, withdrawAmount)) {
+                    if (accountService.withdraw(withdrawAccountNumber, withdrawAmount)) {
                         userOutputView.displaySuccessMessage("출금", withdrawAmount);
                     } else {
-                        userOutputView.displayErrorMessage("잔액 부족");
+                        userOutputView.displayErrorMessage("잔액 부족 또는 계좌 오류");
                     }
                     break;
                 case 3: // 계좌 이체
-                    //TODO: 계좌 이체 로직은 여기에 구현, 실제 애플리케이션에서는 이체할 계좌의 입력도 필요
+                    userOutputView.displayMessage("이체할 계좌의 번호를 입력해주세요:");
+                    String fromAccountNumber = userInputView.getAccountNumber();
+                    userOutputView.displayMessage("이체 받을 계좌의 번호를 입력해주세요:");
+                    String toAccountNumber = userInputView.getAccountNumber();
+                    double transferAmount = userInputView.getAmount();
+                    if (accountService.transfer(fromAccountNumber, toAccountNumber, transferAmount)) {
+                        userOutputView.displaySuccessMessage("이체", transferAmount);
+                    } else {
+                        userOutputView.displayErrorMessage("이체 실패: 잔액 부족 또는 계좌 오류");
+                    }
                     break;
                 case 4: // 잔액 조회
-                    double balance = accountService.checkBalance(account);
+                    userOutputView.displayMessage("조회할 계좌의 번호를 입력해주세요:");
+                    String balanceAccountNumber = userInputView.getAccountNumber();
+                    double balance = accountService.checkBalance(balanceAccountNumber);
                     userOutputView.displayBalance(balance);
                     break;
                 case 0: // 종료
@@ -54,4 +60,3 @@ public class BankController {
         }
     }
 }
-
