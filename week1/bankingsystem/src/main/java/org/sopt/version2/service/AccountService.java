652d91e69196c5f81c53a0df91453c5fc4614e39
권장순 <jsoonworld@gmail.com>
2024-04-05 22:15:35 +0900

    feat: AccountService 구현

---
+++ week1/bankingsystem/src/main/java/org/sopt/version2/service/AccountService.java
@@ -0,0 +1,53 @@
+package org.sopt.version2.service;
+
+import org.sopt.version2.domain.Account;
+import org.sopt.version2.error.ErrorMessage;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AccountService {
+    private final List<Account> accounts = new ArrayList<>();
+
+    public AccountService() {
+        accounts.add(new Account("123-456-789", 0));
+        accounts.add(new Account("987-654-321", 0));
+    }
+
+
+    private Account findAccountByNumber(String accountNumber) {
+        return accounts.stream()
+                .filter(account -> account.getAccountNumber().equals(accountNumber))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_ACCOUNT_NUMBER.toString()));
+    }
+
+
+    public void deposit(String accountNumber, Integer amount) {
+        Account account = findAccountByNumber(accountNumber);
+        account.deposit(amount);
+    }
+
+
+    public void withdraw(String accountNumber, Integer amount) {
+        Account account = findAccountByNumber(accountNumber);
+        account.withdraw(amount);
+    }
+
+
+    public void transfer(String fromAccountNumber, String toAccountNumber, Integer amount) {
+        Account fromAccount = findAccountByNumber(fromAccountNumber);
+        Account toAccount = findAccountByNumber(toAccountNumber);
+        if (fromAccount.getBalance() < amount) {
+            throw new IllegalArgumentException(ErrorMessage.INSUFFICIENT_BALANCE.toString());
+        }
+        fromAccount.withdraw(amount);
+        toAccount.deposit(amount);
+    }
+
+
+    public Integer checkBalance(String accountNumber) {
+        Account account = findAccountByNumber(accountNumber);
+        return account.getBalance();
+    }
+}
