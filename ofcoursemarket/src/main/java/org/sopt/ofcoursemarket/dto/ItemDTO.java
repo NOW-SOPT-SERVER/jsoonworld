ef7a0615be09e2718afd7fdf3e5b81c76ac40988
권장순 <jsoonworld@gmail.com>
2024-04-15 20:40:04 +0900

    feat: ItemDTO

---
+++ ofcoursemarket/src/main/java/org/sopt/ofcoursemarket/dto/ItemDTO.java
@@ -0,0 +1,19 @@
+package org.sopt.ofcoursemarket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sopt.ofcoursemarket.domain.TradeType;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDTO {
+
+    private String title;
+    private String description;
+    private Double price;
+    private TradeType tradeType;
+    private Boolean isPriceNegotiable;
+    private String preferredTradingLocation;
+}
