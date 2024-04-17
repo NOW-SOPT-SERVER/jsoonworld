e528245ddbf71d2e82d6560c3fb484d50cbfe59d
권장순 <jsoonworld@gmail.com>
2024-04-17 16:40:55 +0900

    feat: 모든 회원 조회 테스트코드 구현

--- week2/api/src/test/java/org/sopt/api/controller/MemberControllerTest.java
+++ week2/api/src/test/java/org/sopt/api/controller/MemberControllerTest.java
@@ -5,14 +5,18 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.sopt.api.domain.Member;
 import org.sopt.api.domain.Part;
 import org.sopt.api.repository.MemberRepository;
 import org.sopt.api.service.MemberService;
 import org.sopt.api.service.dto.MemberCreateDto;
+import org.sopt.api.service.dto.MemberFindDto;
 import org.sopt.api.settings.ApiTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -50,5 +54,43 @@ class MemberControllerTest extends ApiTest {
             //then
             Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         }
+    }
+
+    @Nested
+    @DisplayName("모든 멤버 조회 테스트")
+    public class FindAllMembers {
+
+        @Test
+        @DisplayName("모든 멤버 조회 성공")
+        public void findAllMemberSuccess() {
+
+            //given
+            List<MemberFindDto> expectedMembers = List.of(
+                    new MemberFindDto("남궁찬", Part.DESIGN, 30),
+                    new MemberFindDto("강현욱", Part.IOS, 21),
+                    new MemberFindDto("양대한", Part.ANDROID, 27)
+            );
+            memberRepository.saveAll(List.of(
+                    new Member("남궁찬", Part.DESIGN, 30),
+                    new Member("강현욱", Part.IOS, 21),
+                    new Member("양대한", Part.ANDROID, 27)
+            ));
+
+            //when
+            final var response = RestAssured
+                    .given()
+                    .log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when()
+                    .get("/api/v1/member/all")
+                    .then().log().all().extract();
+
+            //then
+            Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            Assertions.assertThat(
+                    response.jsonPath().getList("", MemberFindDto.class))
+                    .usingRecursiveFieldByFieldElementComparator().isEqualTo(expectedMembers);
+        }
+
     }
 }
