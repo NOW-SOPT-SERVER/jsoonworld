0ebd5e38bcf3367f750686c8e5a85972142a4956
권장순 <jsoonworld@gmail.com>
2024-04-17 16:05:37 +0900

    feat: MemberControllerTest

---
+++ week2/api/src/test/java/org/sopt/api/controller/MemberControllerTest.java
@@ -0,0 +1,54 @@
+package org.sopt.api.controller;
+
+import io.restassured.RestAssured;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.sopt.api.domain.Part;
+import org.sopt.api.repository.MemberRepository;
+import org.sopt.api.service.MemberService;
+import org.sopt.api.service.dto.MemberCreateDto;
+import org.sopt.api.settings.ApiTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberControllerTest extends ApiTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Nested
+    @DisplayName("멤버 생성 테스트")
+    public class CreateMember {
+
+        @Test
+        @DisplayName("요청 성공 테스트")
+        public void createMemberSuccess() throws Exception {
+            //given
+            final var request = new MemberCreateDto(
+                    "권장순",
+                    Part.SERVER,
+                    26
+            );
+
+            //when
+            final var response = RestAssured
+                    .given()
+                    .log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .when()
+                    .post("/api/v1/member")
+                    .then().log().all().extract();
+            //then
+            Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        }
+    }
+}
