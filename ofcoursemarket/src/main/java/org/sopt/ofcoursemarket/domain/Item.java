99a87d6a884061bf0eafc07ac2da7e1df4d059a0
권장순 <jsoonworld@gmail.com>
2024-04-17 22:01:55 +0900

    feat: Item

---
+++ ofcoursemarket/src/main/java/org/sopt/ofcoursemarket/domain/Item.java
@@ -0,0 +1,40 @@
+package org.sopt.ofcoursemarket.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "items")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TradeType tradeType;
+
+    @Column(nullable = false)
+    private Boolean isPriceNegotiable;
+
+    @Column(nullable = false)
+    private String preferredTradingLocation;
+
+}
