5964df4f5304be49f091aabaaca519e3fa5bfbd6
권장순 <jsoonworld@gmail.com>
2024-04-17 20:19:43 +0900

    feat: main 로직 작성 및 h2 연결 확인

--- week2/api/src/main/java/org/sopt/api/ApiApplication.java
+++ week2/api/src/main/java/org/sopt/api/ApiApplication.java
@@ -1,13 +1,43 @@
 package org.sopt.api;
 
+import org.sopt.api.domain.Member;
+import org.sopt.api.domain.Part;
+import org.sopt.api.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			// 멤버 생성 및 저장
+			memberRepository.save(new Member("남궁찬", Part.DESIGN, 30));
+			memberRepository.save(new Member("강현욱", Part.IOS, 21));
+			memberRepository.save(new Member("양대한", Part.ANDROID, 27));
+
+			// 모든 멤버 정보 조회
+			String url = "http://localhost:8080/api/v1/member/all";
+			String response = restTemplate.getForObject(url, String.class);
+			System.out.println("Response from API: " + response);
+		};
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
