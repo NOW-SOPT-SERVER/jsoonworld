0fb99b26f871e975f7cdbe33aa9852332bfd2b00
권장순 <jsoonworld@gmail.com>
2024-05-01 19:22:33 +0900

    feat: PostController#findPosts()

--- week3/practice/src/main/java/org/sopt/practice/controller/PostController.java
+++ week3/practice/src/main/java/org/sopt/practice/controller/PostController.java
@@ -6,10 +6,13 @@ import org.sopt.practice.common.dto.SuccessMessage;
 import org.sopt.practice.common.dto.SuccessStatusResponse;
 import org.sopt.practice.service.service.PostService;
 import org.sopt.practice.service.service.dto.PostCreateRequest;
+import org.sopt.practice.service.service.dto.PostFindAllDto;
 import org.sopt.practice.service.service.dto.PostFindDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -34,5 +37,12 @@ public class PostController {
             return ResponseEntity.status(HttpStatus.OK).body(SuccessStatusResponse.of(
                 SuccessMessage.POST_FIND_SUCCESS,
                 postService.findPostById(postId)));
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<SuccessStatusResponse<List<PostFindAllDto>>> findPosts() {
+        return ResponseEntity.status(HttpStatus.OK).body(SuccessStatusResponse.of(
+                SuccessMessage.POST_FIND_SUCCESS,
+                postService.findAllPosts()));
     }
 }
