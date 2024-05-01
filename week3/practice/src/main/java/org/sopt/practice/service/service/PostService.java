24b7483973f1d3689200c124effb59e31788119f
권장순 <jsoonworld@gmail.com>
2024-05-01 12:50:48 +0900

    feat: PostService#create()

---
+++ week3/practice/src/main/java/org/sopt/practice/service/service/PostService.java
@@ -0,0 +1,43 @@
+package org.sopt.practice.service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.practice.common.dto.ErrorMessage;
+import org.sopt.practice.domain.Blog;
+import org.sopt.practice.domain.Post;
+import org.sopt.practice.exception.NotFoundException;
+import org.sopt.practice.repository.BlogRepository;
+import org.sopt.practice.repository.PostRepository;
+import org.sopt.practice.service.service.dto.PostCreateRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final BlogRepository blogRepository;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public String create(Long memberId, Long blogId, PostCreateRequest postCreateRequest) {
+
+        validateBlogOwnership(memberId, blogId);
+
+        Blog blog = blogRepository.findById(blogId).orElseThrow(
+                () -> new NotFoundException(ErrorMessage.BLOG_NOT_FOUND)
+        );
+
+        Post post = postRepository.save(Post.create(blog, postCreateRequest.title(), postCreateRequest.content()));
+        return post.getId().toString();
+
+    }
+
+    private void validateBlogOwnership(Long memberId, Long blogId) {
+        Blog blog = blogRepository.findById(blogId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.BLOG_NOT_FOUND));
+
+        if(!blog.getMember().getId().equals(memberId)) {
+            throw new ForbiddenException(ErrorMessage.FORBIDDEN_MEMBER_ACCESS);
+        }
+    }
+}
