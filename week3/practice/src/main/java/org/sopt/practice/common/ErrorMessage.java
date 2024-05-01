6939e8375e8994ad4684895cd5961c6cb49625b6
권장순 <jsoonworld@gmail.com>
2024-05-01 11:47:24 +0900

    feat: ErrorMessage에 포스트 에러 enum 추가

--- week3/practice/src/main/java/org/sopt/practice/common/ErrorMessage.java
+++ week3/practice/src/main/java/org/sopt/practice/common/ErrorMessage.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorMessage {
     MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 사람이 없습니다"),
-    BLOG_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "[ERROR] 타이틀 오류");
+    BLOG_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "[ERROR] 타이틀 오류"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 ID의 포스트를 찾을 수 없습니다.");
+
 
     private final int status;
     private final String message;
