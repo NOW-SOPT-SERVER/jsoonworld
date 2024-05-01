bba65f7fe0d81445f3984b5eaa4e3bcde0be415b
권장순 <jsoonworld@gmail.com>
2024-05-01 11:44:44 +0900

    feat: PostRepository#findByIdOrThrow() 구현

---
+++ week3/practice/src/main/java/org/sopt/practice/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package org.sopt.practice.repository;
+
+import org.sopt.practice.domain.Post;
+import org.sopt.practice.exception.NotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    default Post findByIdOrThrow(Long id) {
+        return findById(id).orElseThrow(
+                () -> new NotFoundException(ErrorMessage.POST_NOT_FOUND)
+        );
+    }
+}
