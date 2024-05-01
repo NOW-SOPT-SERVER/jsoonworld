b1b74a8968897972212551c9111d6a59364a282e
권장순 <jsoonworld@gmail.com>
2024-05-01 19:43:13 +0900

    feat: GlobalExceptionHandler#handleDataAccessException()

--- week3/practice/src/main/java/org/sopt/practice/common/GlobalExceptionHandler.java
+++ week3/practice/src/main/java/org/sopt/practice/common/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.sopt.practice.common;
 import org.sopt.practice.common.dto.ErrorMessage;
 import org.sopt.practice.common.dto.ErrorResponse;
 import org.sopt.practice.exception.NotFoundException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,6 +14,7 @@ import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<ErrorResponse> handleEntityNotFoundException(NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(HttpStatus.NOT_FOUND.value(), ErrorMessage.MEMBER_NOT_FOUND_BY_ID_EXCEPTION.getMessage()));
@@ -22,4 +24,11 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()));
     }
+
+    @ExceptionHandler(DataAccessException.class)
+    protected ResponseEntity<ErrorResponse> handleDataAccessException(DataAccessException e) {
+        ErrorResponse response = ErrorResponse.of(ErrorMessage.DATABASE_ACCESS_ERROR.getStatus(), ErrorMessage.DATABASE_ACCESS_ERROR.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
 }
--- week3/practice/src/main/java/org/sopt/practice/common/dto/ErrorMessage.java
+++ week3/practice/src/main/java/org/sopt/practice/common/dto/ErrorMessage.java
@@ -10,7 +10,8 @@ public enum ErrorMessage {
     MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "[ERROR] ID에 해당하는 사람이 없습니다"),
     BLOG_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "[ERROR] 타이틀 오류"),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "[ERROR] 해당 ID의 포스트를 찾을 수 없습니다."),
-    FORBIDDEN_MEMBER_ACCESS(HttpStatus.FORBIDDEN.value(), "[ERROR] 해당 블로그에 대한 접근 권한이 없습니다.");
+    FORBIDDEN_MEMBER_ACCESS(HttpStatus.FORBIDDEN.value(), "[ERROR] 해당 블로그에 대한 접근 권한이 없습니다."),
+    DATABASE_ACCESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "[ERROR] 데이터베이스 접근 중 오류 발생");
 
 
 
