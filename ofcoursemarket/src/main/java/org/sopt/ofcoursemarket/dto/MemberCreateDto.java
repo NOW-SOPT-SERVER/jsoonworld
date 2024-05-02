979cf3d01a0f3a5e2f3b2b2e8df6f6fc3162586a
권장순 <jsoonworld@gmail.com>
2024-05-02 14:02:13 +0900

    del: Pre ofcoursemarket project for reset

--- ofcoursemarket/src/main/java/org/sopt/ofcoursemarket/dto/MemberCreateDto.java
+++
@@ -1,3 +0,0 @@
-package org.sopt.ofcoursemarket.dto;
-
-public record MemberCreateDto(String userId, String name, int age) {}
--- ofcoursemarket/src/main/java/org/sopt/ofcoursemarket/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.ofcoursemarket.repository;
-
-import org.sopt.ofcoursemarket.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
